24485fd318342e87edb534e998bc43097c9df70d
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-02 11:44:03 -0800

    add colored text

--- as0/src/as0.cpp
+++ as0/src/as0.cpp
@@ -7,23 +7,41 @@ int main() {
     raylib::Window window(800, 400, "CS381 - Assignment 0");
     window.SetState(FLAG_WINDOW_RESIZABLE);
 
-    raylib::Text text;
-    text.SetText("Congratulations! Assignment 0 is complete!");
-    text.SetFontSize(30);
-    text.SetColor(raylib::Color::Black());
+    raylib::Text textStringOne;
+    raylib::Text textStringTwo;
+    raylib::Text textStringThree;
+
+    textStringOne.SetText("Congratulations!");
+    textStringOne.SetFontSize(24);
+    textStringOne.SetColor(raylib::Color::Red());
+
+    textStringTwo.SetText("Assignment 0 is");
+    textStringTwo.SetFontSize(24);
+    textStringTwo.SetColor(raylib::Color::Black());
+
+    textStringThree.SetText("complete!");
+    textStringThree.SetFontSize(24);
+    textStringThree.SetColor(raylib::Color::DarkGreen());
 
     while(!window.ShouldClose()) {
         int width = window.GetWidth();
         int height = window.GetHeight();
-        int textWidth = text.Measure();
-        int textHeight = text.GetFontSize();
+        int textOneWidth = textStringOne.Measure();
+        int textTwoWidth = textStringTwo.Measure();
+        int textThreeWidth = textStringThree.Measure();
+        int textHeight = textStringOne.GetFontSize();
 
-        float textXPosition = (width - textWidth) / 2;
+        float totalWidth = textOneWidth + textTwoWidth + textThreeWidth;
+        float textXPosition = (width - totalWidth) / 2;
         float textYPosition = (height - textHeight) / 2;
+
+        float textGap = 20.0f;
 
         window.BeginDrawing();
             window.ClearBackground(raylib::Color::White());
-            text.Draw({textXPosition, textYPosition});
+            textStringOne.Draw({textXPosition, textYPosition});
+            textStringTwo.Draw({textXPosition + textOneWidth, textYPosition});
+            textStringThree.Draw({textXPosition + textOneWidth + textTwoWidth - textGap, textYPosition});
         window.EndDrawing();
     }
 
