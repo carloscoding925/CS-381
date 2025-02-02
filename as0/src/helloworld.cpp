7b742639e80e688cb3194e4922564375355a6e90
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-02 00:30:05 -0800

    doot

--- as0/src/helloworld.cpp
+++ as0/src/helloworld.cpp
@@ -1,12 +1,20 @@
+#include "Color.hpp"
 #include <iostream>
+
 #include <raylib-cpp.hpp>
 
 int main() {
     raylib::Window window(800, 400, "Hello World");
     window.SetState(FLAG_WINDOW_RESIZABLE);
 
+    raylib::Text text;
+    text.SetText("Hello World!");
+    text.SetFontSize(30);
+
     while(!window.ShouldClose()) {
         window.BeginDrawing();
+            window.ClearBackground(raylib::Color::Blue());
+            text.Draw({100, 0});
         window.EndDrawing();
     }
 
