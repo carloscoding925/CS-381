ff0f0f54521a0d59ce39b0ad8f63539040f88b2d
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-07 23:28:18 -0800

    compiling compiling compiling

---
+++ as1/src/as1.cpp
@@ -0,0 +1,11 @@
+#include <raylib-cpp.hpp>
+#include "VolumeControl.h"
+
+void PingButton() {
+    // Do something
+    TraceLog(LOG_INFO, "PingButton() called");
+}
+
+int main() {
+    return 0;
+}
---
+++ as1/src/implementations.cpp
@@ -0,0 +1,8 @@
+extern void PingButton();
+
+#define RAYGUI_IMPLEMENTATION
+#define GUI_VOLUMECONTROL_IMPLEMENTATION
+#include "VolumeControl.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
