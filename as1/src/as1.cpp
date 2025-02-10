6f20d47688778b799a4c070c334544521c551e63
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-09 19:53:37 -0800

    sounds

--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -18,8 +18,8 @@ int main() {
     raylib::AudioDevice audioDevice; 
     audioDevice.Init();
     Sound ping = LoadSound("../audio/get-out-0.5.mp3");
-    raylib::Music backgroundNoise = LoadMusicStream("../audio/ocean-waves.mp3");
-    raylib::Music music = LoadMusicStream("../audio/coffeeshop.mp3");
+    raylib::Music backgroundNoise = LoadMusicStream("../audio/crowd.wav");
+    raylib::Music music = LoadMusicStream("../audio/coffeeshop-music.mp3");
 
     // Set audio to loop
     backgroundNoise.SetLooping(true);
