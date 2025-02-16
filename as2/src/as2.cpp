92b9b0a2e9654f8e38434f88e93296c213506bb6
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-16 11:58:03 -0800

    window

--- as2/src/as2.cpp
+++ as2/src/as2.cpp
@@ -1,4 +1,15 @@
+#include <raylib-cpp.hpp>
+
 int main() {
+    raylib::Window window(800, 400, "CS381 - Assignment 2");
+
+    while(!window.ShouldClose()) {
+        window.BeginDrawing();
+        {
+            
+        }
+        window.EndDrawing();
+    }
 
     return 0;
 }
