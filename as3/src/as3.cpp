736e5cb791c202b59e5f38f756afabc8c9be0f67
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-22 18:43:20 -0800

    cone

--- as3/src/as3.cpp
+++ as3/src/as3.cpp
@@ -1,23 +1,71 @@
+#include "Color.hpp"
+#include "Text.hpp"
+#include "Window.hpp"
 #include <raylib-cpp.hpp>
 #include "../assets/skybox.hpp"
+#include <iostream>
+
+template<typename T>
+concept Transformer = requires(T t, raylib::Matrix m) {
+    { t(m) } -> std::convertible_to<raylib::Matrix>;
+};
+
+void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Matrix backup = model.transform;
+    model.transform = transformer(backup);
+    model.Draw({});
+    auto box = model.GetTransformedBoundingBox();
+    DrawBoundingBox(box, raylib::Color::Black());
+    model.transform = backup;
+}
 
 int main() {
-    raylib::Window window(1000, 700, "CS381 - Assignment 3");
+    const int screenWidth = 1000;
+    const int screenHeight = 700;
+    const std::string title = "CS381 - Assignment 3";
+
+    raylib::Window window(screenWidth, screenHeight, title);
+    window.SetState(FLAG_WINDOW_RESIZABLE);
+
+    raylib::Model model("../assets/Kenny Car Kit/cone.glb");
+    model.transform = raylib::Matrix::Identity().Scale(5);
+
+    raylib::Model grass = raylib::Mesh::Plane(100, 100, 1, 1).LoadModelFrom();
+    raylib::Texture grassTexture = raylib::Texture("../assets/textures/grass.jpg");
+    grass.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassTexture;
+
+    float targetSpeed = 5;
+    float coneHeading = 45;
+    float coneSpeed = 0;
+    raylib::Vector3 conePosition = { 0.0f, 0.0f, 0.0f };
+
     cs381::SkyBox sky("textures/skybox.png");
-    auto camera = raylib::Camera(
-        {0, 120, 500},
-        {0, 0, 0},
-        {0, 1, 0},
-        45
-    );
+    raylib::Camera3D camera(raylib::Vector3{0.0f, 10.0f, 30.0f}, raylib::Vector3{0.0f, 0.0f, 0.0f}, raylib::Vector3{0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE);
+
+    float timer = 0;
 
     while(!window.ShouldClose()) {
+
+        coneSpeed = std::lerp(coneSpeed, targetSpeed, window.GetFrameTime());
+        raylib::Vector3 velocity = { cos(raylib::Degree(coneHeading)) * coneSpeed, 0, -sin(raylib::Degree(coneHeading)) * coneSpeed };
+        conePosition = conePosition + velocity * window.GetFrameTime();
+
         window.BeginDrawing();
+        {
+            window.ClearBackground(raylib::Color::White());
             camera.BeginMode();
-                window.ClearBackground(raylib::Color::White());
+            {
                 sky.Draw();
+                grass.Draw({});
+                DrawBoundedModel(model, [&conePosition, &coneHeading](raylib::Transform t) {
+                    return t.Translate(conePosition).RotateY(raylib::Degree(coneHeading));
+                });
+            }
             camera.EndMode();
+
+        }
         window.EndDrawing();
+
     }
 
     return 0;
