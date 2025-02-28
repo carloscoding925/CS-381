f77783b1ed198c52626963230ec52a1383b63067
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-27 16:24:24 -0800

    load custom textures

--- as4/src/as4.cpp
+++ as4/src/as4.cpp
@@ -27,11 +27,35 @@ int main() {
     raylib::Window window(windowWidth, windowHeight, title);
     window.SetState(FLAG_WINDOW_RESIZABLE);
 
+    raylib::Model toilet("../assets/Kenny-Furniture-Kit/toilet.glb");
+    toilet.transform = raylib::Matrix::Identity().Scale(5);
+    raylib::Model hotdog("../assets/Kenny-Food-Kit/hot-dog-raw.glb");
+    hotdog.transform = raylib::Matrix::Identity().Scale(5);
+
+    raylib::Model grass = raylib::Mesh::Plane(100, 100, 1, 1).LoadModelFrom();
+    raylib::Texture grassTexture = raylib::Texture("../assets/textures/grass.png");
+    grass.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassTexture;
+
+    cs381::SkyBox sky("textures/skybox.png");
+    raylib::Camera3D camera(raylib::Vector3{0.0f, 10.0f, 30.0f}, raylib::Vector3{0.0f, 0.0f, 0.0f}, raylib::Vector3{0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE);
+
     while(!window.ShouldClose()) {
 
         window.BeginDrawing();
         {
             window.ClearBackground(raylib::Color::White());
+            camera.BeginMode();
+            {
+                sky.Draw();
+                grass.Draw({});
+                DrawBoundedModel(toilet, [](raylib::Transform t) {
+                    return t.Translate(raylib::Vector3{0.0f, 0.0f, 0.0f});
+                });
+                DrawBoundedModel(hotdog, [](raylib::Transform t) {
+                    return t.Translate(raylib::Vector3{0.0f, 0.0f, 0.0f});
+                });
+            }
+            camera.EndMode();
         }
         window.EndDrawing();
 
