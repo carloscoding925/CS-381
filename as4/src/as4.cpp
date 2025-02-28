86a4696c6a49376d8663dc6c8549695d7f044126
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-27 16:10:31 -0800

    import assets

--- as4/src/as4.cpp
+++ as4/src/as4.cpp
@@ -6,10 +6,24 @@
 #include <iostream>
 #include <raymath.h>
 
+template<typename T>
+concept Transformer = requires(T t, raylib::Matrix m) {
+    { t(m) } -> std::convertible_to<raylib::Matrix>;
+};
+
+void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Matrix backup = model.transform;
+    model.transform = transformer(backup);
+    model.Draw({});
+    auto box = model.GetTransformedBoundingBox();
+    DrawBoundingBox(box, raylib::Color::Black());
+    model.transform = backup;
+}
+
 int main() {
     const int windowWidth = 1000;
     const int windowHeight = 700;
-    const std::string title = "CS381 - Assignment 4";
+    const std::string title = "Skibidi Toilet Glizzy Gobler";
     raylib::Window window(windowWidth, windowHeight, title);
     window.SetState(FLAG_WINDOW_RESIZABLE);
 
