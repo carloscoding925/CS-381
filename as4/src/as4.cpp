7ce8b2a790765ffdf39aec72f663321e76bafa3d
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-28 20:10:23 -0800

    spawn the dawgs

--- as4/src/as4.cpp
+++ as4/src/as4.cpp
@@ -20,27 +20,42 @@ void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
     model.transform = backup;
 }
 
+bool checkCollision(raylib::Vector3& toilet, raylib::Vector3& hotdog) {
+    float radius = 5.0f;
+    if (std::fabs(toilet.x - hotdog.x) < radius && std::fabs(toilet.z - hotdog.z) < radius) {
+        return true;
+    }
+
+    return false;
+}
+
+raylib::Vector3 generateNewLocation() {
+    float x = std::rand() % 200 - 100;
+    float z = std::rand() % 200 - 100;
+    return raylib::Vector3{x, 0.0f, z};
+}
+
 int main() {
     const int windowWidth = 1000;
     const int windowHeight = 700;
-    const std::string title = "The Rizzler & Big AJ Present: Skibidi Toilet Glizzy Gobler";
+    const std::string title = "The Rizzler & Big AJ Present: Skibidi Toilet Glizzy Gobbler";
     raylib::Window window(windowWidth, windowHeight, title);
     window.SetState(FLAG_WINDOW_RESIZABLE);
 
     raylib::Model toilet("../assets/Kenny-Furniture-Kit/toilet.glb");
     toilet.transform = raylib::Matrix::Identity().Scale(5);
-    raylib::Model hotdog("../assets/Kenny-Food-Kit/hot-dog-raw.glb");
-    hotdog.transform = raylib::Matrix::Identity().Scale(5);
+    raylib::Model hotdog("../assets/Kenny-Food-Kit/meat-sausage.glb");
+    hotdog.transform = raylib::Matrix::Identity().Scale(15);
 
-    raylib::Model grass = raylib::Mesh::Plane(100, 100, 1, 1).LoadModelFrom();
-    raylib::Texture grassTexture = raylib::Texture("../assets/textures/grass.png");
-    grass.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassTexture;
+    raylib::Model floor = raylib::Mesh::Plane(200, 200, 1, 1).LoadModelFrom();
+    raylib::Texture floorTexture = raylib::Texture("../assets/textures/floor.png");
+    floor.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = floorTexture;
 
     cs381::SkyBox sky("textures/skybox.png");
     raylib::Camera3D camera(raylib::Vector3{0.0f, 20.0f, 40.0f}, raylib::Vector3{0.0f, 0.0f, 0.0f}, raylib::Vector3{0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE);
 
     raylib::Vector3 toiletPosition = { 0.0f, 0.0f, 0.0f };
-    raylib::Vector3 hotdogPosition = { 0.0f, 0.0f, 0.0f };
+    raylib::Vector3 hotdogPosition = { 0.0f, 0.0f, -50.0f };
 
     float targetSpeed = 0;
     float toiletSpeed = 0;
@@ -48,19 +63,24 @@ int main() {
     float targetHeading = 0;
     float toiletYTarget = 0;
 
+    int hotdogCounter = 0;
+
     while(!window.ShouldClose()) {
         if (raylib::Keyboard::IsKeyDown(KEY_W)) {
-            targetSpeed = 5.0f;
+            targetSpeed = 20.0f;
+        }
+        else if (raylib::Keyboard::IsKeyDown(KEY_S)) {
+            targetSpeed = -20.0f;
         } 
         else {
             targetSpeed = 0.0f;
         }
 
         if (raylib::Keyboard::IsKeyDown(KEY_A)) {
-            targetHeading = targetHeading + 0.02f;
+            targetHeading = targetHeading + 0.04f;
         }
         else if (raylib::Keyboard::IsKeyDown(KEY_D)) {
-            targetHeading = targetHeading - 0.02;
+            targetHeading = targetHeading - 0.04;
         }
 
         if (raylib::Keyboard::IsKeyPressed(KEY_SPACE)) {
@@ -74,12 +94,16 @@ int main() {
         toiletSpeed = std::lerp(toiletSpeed, targetSpeed, window.GetFrameTime());
         toiletHeading = std::lerp(toiletHeading, targetHeading, window.GetFrameTime());
         toiletPosition.y = std::lerp(toiletPosition.y, toiletYTarget, window.GetFrameTime() * 2);
-        std::cout << toiletHeading << std::endl;
         raylib::Vector3 velocity = { cos(radians) * toiletSpeed, 0, -sin(radians) * toiletSpeed };
         toiletPosition = toiletPosition + velocity * window.GetFrameTime();
 
         camera.position = toiletPosition + raylib::Vector3{0.0f, 20.0f, 40.0f};
         camera.target = toiletPosition;
+
+        if (checkCollision(toiletPosition, hotdogPosition)) {
+            hotdogPosition = generateNewLocation();
+            hotdogCounter++;
+        }
 
         window.BeginDrawing();
         {
@@ -87,7 +111,7 @@ int main() {
             camera.BeginMode();
             {
                 sky.Draw();
-                grass.Draw({});
+                floor.Draw({});
                 DrawBoundedModel(toilet, [&toiletPosition, &toiletHeading](raylib::Transform t) {
                     return t.Translate(toiletPosition).RotateY(raylib::Degree(toiletHeading));
                 });
