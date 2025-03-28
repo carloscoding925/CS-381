2178005a3c536c27513b5ab8c117599a69d4dfac
Carlos Hernandez <carlosandresh925@gmail.com>
2025-03-27 20:52:06 -0700

    fisix component rework

--- as7/src/CO.hpp
+++ as7/src/CO.hpp
@@ -1,54 +1,30 @@
-// Preprocessor directive - ensures file is only included once
 #pragma once
 
-// Macro for OIS support in raylib
-#define RAYLIB_SUPPORT_OIS
-// Include the raylib library
 #include <raylib-cpp.hpp>
 
-// Include memory header file from the standard library
 #include <memory>
-// Include vector header file from the standard library
 #include <vector>
-// Include optional header file from the standard library
 #include <optional>
 
-// Defines a set of related functions, classes, structs etc.
 namespace cs381 {
 
 	/**
 	 * @brief Base class for various components that can be attached to an entity.
 	 */
-	// Component parent class declaration
 	class Component {
-		// Declares a pointer to an Entity struct. This references the entity that the component is attached to.
 		struct Entity* object;
-		/* Using the 'friend' keyword allows the Entity struct to access any potential private or protected members of future
-		 classes that inherit from the Component class. */
 		friend struct Entity;
 
-	// Methods accessible from outside the component class
 	public:
-		// Returns a reference to the entity which the component is attached ti
 		struct Entity& Object() { return *object; }
-		// Returns a reference to the transform component of the entity
 		struct TransformComponent& Transform();
-		// Bool for determining if the component should be active
 		bool enabled = true;
 
-		// Constructor for the Component class - Accepts a reference to an Entity struct
 		Component(struct Entity& e) : object(&e) {}
-		// Constructor for the Component class - Accepts a reference to an Entity struct and a boolean for determining if the component should be active
 		Component(struct Entity& e, bool enabled): object(&e), enabled(enabled) {} // Start
-		/* Virtual method meant to be overridden by child classes
-		When overwritten in child classes, actions to be performed are defined in here */
 		virtual void Tick(float dt) {}
-		// Destructor for the component class
-		// Currenly does nothing, meant to be overridden by child classes to clean up memory
 		virtual ~Component() {} // Cleanup
 
-		// Template function - casts the component to a derived type T and returns a reference to it
-		// dynamic_cast casts the component to type T, and return a reference if the cast is successful, otherwise returns a nullptr
 		template<std::derived_from<Component> T>
 		T& as() { return *dynamic_cast<T*>(this); }
 	};
@@ -56,13 +32,9 @@ namespace cs381 {
 	/**
 	 * @brief Component storing the positional data of a Entity
 	 */
-	// Transform Component struct declaration, inherits from the Component class
 	struct TransformComponent: public Component {
-		// Inherits the constructors from the Component class
 		using Component::Component;
-		// Vector3 for storing position
 		raylib::Vector3 position = {0, 0, 0};
-		// Degree for storing object direction
 		raylib::Degree heading = 0;
 	};
 
@@ -70,36 +42,21 @@ namespace cs381 {
 	/**
 	 * @brief Represents an object in the game world.
 	 */
-	// Entity struct declaration
 	struct Entity {
-		// Vector of 'unique_ptr' pointers to components, ensures that each component is exclusive to the entity
 		std::vector<std::unique_ptr<Component>> components = {};
 
-		// Constructor for the Entity struct - also initializes a TransformComponent
-		// Every entity will have a transform component initialized with it
 		Entity() { AddComponent<TransformComponent>(); } // Entities have a transform by default!
-		// Deletes the copy constructor for the Entity struct so they cannot be copied.
 		Entity(const Entity&) = delete;
-
-		// Move constructor for the Entity struct - moves an entities components to another entity
 		Entity(Entity&& other) : components(std::move(other.components)) {
-			// iterate over all our components
 			for(auto& component: components)
-				// Update the object pointer to point to the new entity
 				component->object = this; // When we are moved make sure the components still point to us!
 		}
 
-		// Deletes the copy assignment operator so entities cannot be copied.
 		Entity& operator=(const Entity&) = delete;
-		// Declares a new move assignment operator for the Entity struct
 		Entity& operator=(Entity&& other) {
-			// Move the components from the other entity to this entity
 			components = std::move(other.components);
-			// iterate over all our components
 			for(auto& component: components)
-				// Update the object pointer to point to the new entity
 				component->object = this; // When we are moved make sure the components still point to us!
-			// return the new entity
 			return *this;
 		}
 
@@ -107,13 +64,9 @@ namespace cs381 {
 		 * @brief Performs actions during each game update for the entity and its components.
 		 * @param dt The time elapsed since the last update.
 		 */
-		// Calls the Tick method for each component in the entity
 		void Tick(float dt) {
-			// Iterate through all the components in the entity
 			for(auto& componentPtr: components)
-				// Check if the component is enabled
 				if(componentPtr->enabled)
-					// If the component is enabled, call its Tick method
 					componentPtr->Tick(dt);
 		}
 
@@ -124,15 +77,10 @@ namespace cs381 {
 		 * @param args The arguments to pass to the component's constructor.
 		 * @return The index of the added component.
 		 */
-		// Template declaration, checks to make sure T is derived from the Component class, with an N number of arguments
 		template<std::derived_from<Component> T, typename... Ts>
-		// Function declaration for adding a component to an entity, returns a size_t and accepts N number of arguments
 		size_t AddComponent(Ts... args) {
-			// Creates a new unique pointer to a component of type T, and passes the entity and arguments to the constructor
 			std::unique_ptr<Component> component = std::make_unique<T>(*this, std::forward<Ts>(args) ...);
-			// Adds the component to the entity's components vector
 			components.push_back(std::move(component));
-			// Returns the index of the added component
 			return components.size() - 1;
 		}
 
@@ -143,25 +91,18 @@ namespace cs381 {
 		 * @tparam T The type of component to retrieve.
 		 * @return An optional reference to the component if found, or std::nullopt if not found.
 		 */
-		// Template declaration, checks to make sure T is derived from the Component class
 		template<std::derived_from<Component> T>
-		// Function declaration for retrieving a component from an entity, returns an optional reference to the component
 		std::optional<std::reference_wrapper<T>> GetComponent() {
-			// Check if the component is a TransformComponent
 			if constexpr(std::is_same_v<T, TransformComponent>){ // This extra step here allows us to skip itterator overhead since we know the transform component should be in slot 0
-				// Attempt to cast the component to type T
 				T* cast = dynamic_cast<T*>(components[0].get());
-				// If the cast is successful, return a reference to the component
 				if(cast) return *cast;
 			}
-			// Iterate through all the components in the entity
+
 			for(auto& componentPtr: components){
-				// Attempt to cast the component to type T
 				T* cast = dynamic_cast<T*>(componentPtr.get());
-				// If the cast is successful, return a reference to the component
 				if(cast) return *cast;
 			}
-			// If the component is not found, return a null optional
+
 			return {};
 		}
 
@@ -170,7 +111,6 @@ namespace cs381 {
 		 * @return The object's transform component.
 		 * @note this function has undefined behavior (probably a crash...) if the transform component is ever removed!
 		 */
-		// Function declaration for retrieving the transform component of an entity
 		TransformComponent& Transform() { return *GetComponent<TransformComponent>(); }
 	};
 
@@ -179,6 +119,5 @@ namespace cs381 {
 	 * @return The object's transform component.
 	 * @note this function has undefined behavior (probably a crash...) if the transform component is ever removed!
 	 */
-	// Function declaration for retrieving the transform component of a component
 	TransformComponent& Component::Transform() { return Object().Transform(); }
 }
--- as7/src/as7.cpp
+++ as7/src/as7.cpp
@@ -5,7 +5,198 @@
 #include "CO.hpp"
 #include <iostream>
 
+template <typename T>
+concept Transformer = requires(T t, raylib::Matrix m) {
+    { t(m) } -> std::convertible_to<raylib::Matrix>;
+};
+
+void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Matrix backup = model.transform;
+    model.transform = transformer(backup);
+    model.Draw({});
+    model.GetTransformedBoundingBox().Draw(raylib::Color::White());
+    model.transform = backup;
+}
+
+void DrawModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Matrix backup = model.transform;
+    model.transform = transformer(backup);
+    model.Draw({});
+    model.transform = backup;
+}
+
+struct MeshRenderComponent: cs381::Component {
+    raylib::Model* model = nullptr;
+    bool drawBoundingBox = false;
+
+    MeshRenderComponent(cs381::Entity& e, raylib::Model* model)
+        : cs381::Component(e), model(model) {}
+
+    void Tick(float dt) override {
+        if (model == nullptr) { return; }
+
+        if (drawBoundingBox) {
+            DrawBoundedModel(*model, [this](raylib::Matrix& matrix){
+                auto& transform = Object().Transform();
+                return matrix.Translate(transform.position).RotateY(transform.heading);
+            });
+        }
+        else {
+            DrawModel(*model, [this](raylib::Matrix& matrix){
+                auto& transform = Object().Transform();
+                return matrix.Translate(transform.position).RotateY(transform.heading);
+            });
+        }
+    }
+};
+
+struct PhysicsProperties {
+    float maxSpeed;
+    float turningSpeed;
+};
+
+struct PhysicsComponent: cs381::Component {
+    PhysicsProperties properties;
+    float speed = 0;
+    bool isRocket = false;
+
+    PhysicsComponent(cs381::Entity& e, const PhysicsProperties& props)
+        : cs381::Component(e), properties(props) {}
+
+    void Tick(float dt) override {
+        if (isRocket) {
+
+        }
+        else {
+            auto& transform = Object().Transform();
+            float radians = DEG2RAD * Object().Transform().heading;
+            speed = std::lerp(speed, properties.maxSpeed, dt);
+            raylib::Vector3 velocity = { cos(radians) * speed, 0, -sin(radians) * speed };
+            transform.position = transform.position + velocity * dt; 
+        }
+    }
+
+};
+
+struct InputComponent: cs381::Component {
+    raylib::BufferedInput* input;
+    size_t* selectedEntityIndex;
+    size_t entityIndex;
+
+    InputComponent(cs381::Entity& e, raylib::BufferedInput* input, size_t* selectedEntityIndex, size_t entityIndex)
+        : cs381::Component(e), input(input), selectedEntityIndex(selectedEntityIndex), entityIndex(entityIndex) {}
+
+    void Tick(float dt) override {
+        (*input)["Forward"].AddPressedCallback([this](){
+            if (*selectedEntityIndex == entityIndex) {
+                
+            }
+        });
+
+        (*input)["Backward"].AddPressedCallback([this](){
+            if (*selectedEntityIndex == entityIndex) {
+
+            }
+        });
+
+        (*input)["Left"].AddPressedCallback([this](){
+            if (*selectedEntityIndex == entityIndex) {
+
+            }
+        });
+
+        (*input)["Right"].AddPressedCallback([this](){
+            if (*selectedEntityIndex == entityIndex) {
+
+            }
+        });
+
+        (*input)["Stop"].AddPressedCallback([this](){
+            if (*selectedEntityIndex == entityIndex) {
+
+            }
+        });
+    }
+};
+
 int main() {
+    const int windowWidth = 1000;
+    const int windowHeight = 700;
+    const std::string title = "CS 381 - Assignment 7";
+    raylib::Window window(windowWidth, windowHeight, title);
+    window.SetState(FLAG_WINDOW_RESIZABLE);
+
+    cs381::SkyBox sky("textures/skybox.png");
+    raylib::Camera3D camera(raylib::Vector3{0.0f, 120.0f, -500.0f}, raylib::Vector3{0.0f, 0.0f, 0.0f}, raylib::Vector3{0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE);
+
+    raylib::Model grass = raylib::Mesh::Plane(1000, 1000, 1, 1).LoadModelFrom();
+    raylib::Texture grassTexture = raylib::Texture("../assets/textures/grass.png");
+    grass.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassTexture;
+
+    raylib::Model rocket("../assets/Kenny Space Kit/rocketA.glb");
+    raylib::Model truck("../assets/Kenny Car Kit/truck.glb");
+
+    rocket.transform = raylib::Matrix::Identity().Scale(30);
+    truck.transform = raylib::Matrix::Identity().Scale(30).RotateY(raylib::Degree(90));
+
+    raylib::BufferedInput input;
+    input["Forward"] = raylib::Action::key(KEY_W).move();
+    input["Backward"] = raylib::Action::key(KEY_S).move();
+    input["Left"] = raylib::Action::key(KEY_A).move();
+    input["Right"] = raylib::Action::key(KEY_D).move();
+    input["Stop"] = raylib::Action::key(KEY_SPACE).move();
+
+    PhysicsProperties entityProperties[] = {
+        { 20.0f, 0.0f },
+        { 20.0f, 0.0f }
+    };
+
+    std::vector<cs381::Entity> entities;
+    size_t selectedEntity = 0;
+
+    cs381::Entity& rocketEntity = entities.emplace_back();
+    rocketEntity.AddComponent<MeshRenderComponent>(&rocket);
+    rocketEntity.AddComponent<PhysicsComponent>(entityProperties[0]);
+    rocketEntity.AddComponent<InputComponent>(&input, &selectedEntity, 0);
+    rocketEntity.GetComponent<cs381::TransformComponent>()->get().position = raylib::Vector3{0.0f, 0.0f, 0.0f};
+
+    cs381::Entity& truckEntity = entities.emplace_back();
+    truckEntity.AddComponent<MeshRenderComponent>(&truck);
+    truckEntity.AddComponent<PhysicsComponent>(entityProperties[1]);
+    truckEntity.AddComponent<InputComponent>(&input, &selectedEntity, 1);
+    truckEntity.GetComponent<cs381::TransformComponent>()->get().position = raylib::Vector3{100.0f, 0.0f, 0.0f};
+
+    entities[selectedEntity].GetComponent<MeshRenderComponent>()->get().drawBoundingBox = true;
+
+    while(!window.ShouldClose()) {
+        window.BeginDrawing();
+        {
+            camera.BeginMode();
+            {
+                window.ClearBackground(raylib::Color::White());
+                sky.Draw();
+                grass.Draw({});
+
+                auto dt = window.GetFrameTime();
+                for(auto& e : entities) {
+                    e.Tick(dt);
+                }
+            }
+            camera.EndMode();
+        }
+        window.EndDrawing();
+
+        if (raylib::Keyboard::IsKeyPressed(KEY_TAB)) {
+            selectedEntity = (selectedEntity + 1) % entities.size();
+            entities[selectedEntity].GetComponent<MeshRenderComponent>()->get().drawBoundingBox = true;
+
+            for (size_t i = 0; i < entities.size(); i++) {
+                if (i != selectedEntity) {
+                    entities[i].GetComponent<MeshRenderComponent>()->get().drawBoundingBox = false;
+                }
+            }
+        }
+    }
 
     return 0;
 }
