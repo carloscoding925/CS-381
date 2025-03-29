669091569b4464508f470b26f0247342d25d909e
Carlos Hernandez <carlosandresh925@gmail.com>
2025-03-29 14:31:13 -0700

    environment

--- as7/src/as7.cpp
+++ as7/src/as7.cpp
@@ -26,6 +26,33 @@ void DrawModel(raylib::Model& model, Transformer auto transformer) {
 }
 
 int main() {
+    const int windowWidth = 1000;
+    const int windowHeight = 700;
+    const std::string title = "CS 381 - Assignment 7";
+    raylib::Window window(windowWidth, windowHeight, title);
+    window.SetState(FLAG_WINDOW_RESIZABLE);
+
+    cs381::SkyBox sky("textures/skybox.png");
+    raylib::Camera3D camera(raylib::Vector3{0.0f, 120.0f, -500.0f}, raylib::Vector3{0.0f, 0.0f, 0.0f}, raylib::Vector3{0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE);
+
+    raylib::Model grass = raylib::Mesh::Plane(1000, 1000, 1, 1).LoadModelFrom();
+    raylib::Texture grassTexture = raylib::Texture("../assets/textures/grass.png");
+    grass.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grassTexture;
+
+    while(!window.ShouldClose()) {
+        window.BeginDrawing();
+        {
+            camera.BeginMode();
+            {
+                window.ClearBackground(raylib::Color::White());
+                sky.Draw();
+                grass.Draw({});
+
+            }
+            camera.EndMode();
+        }
+        window.EndDrawing();
+    }
 
     return 0;
 }
