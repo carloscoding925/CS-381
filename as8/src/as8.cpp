983cb3b95373bd65c91fedfdfb463a927a695ac8
Carlos Hernandez <carlosandresh925@gmail.com>
2025-04-05 18:11:12 -0700

    skibidi toilet

--- as8/src/as8.cpp
+++ as8/src/as8.cpp
@@ -2,6 +2,24 @@
 #include "Window.hpp"
 #include "../assets/skybox.hpp"
 #include <iostream>
+#include "ECS.hpp"
+
+struct Scene {
+    struct PhysicsComponent {
+        raylib::Vector3 position;
+        raylib::Vector3 velocity;
+    };
+
+    PhysicsComponent physics[10];
+    float heading[10];
+    raylib::Model* model[10];
+};
+
+void kinematic_physics(Scene& scene, float dt) {
+    for (int e = 0; e < 10; ++e) {
+        scene.physics[e].position = scene.physics[e].position + scene.physics[e].velocity * dt;
+    }
+}
 
 int main() {
     const int windowWidth = 1000;
@@ -12,6 +30,21 @@ int main() {
 
     cs381::SkyBox sky("textures/skybox.png");
     raylib::Camera3D camera(raylib::Vector3{0.0f, 120.0f, -500.0f}, raylib::Vector3{0.0f, 0.0f, 0.0f}, raylib::Vector3{0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE);
+
+    cs381::Scene<cs381::ComponentStorage> scene;
+    auto e = scene.CreateEntity();
+    scene.AddComponent<raylib::Vector3>(e);
+
+    /*for(cs381::Entity e = 0; e < scene.entityMasks.size(); ++e) {
+        if (!scene.HasComponent<Position>(e)) {
+            continue;
+        }
+        if (!scene.HasComponent<Velocity>(e)) {
+            continue;
+        }
+
+        // do physics on e
+    }*/
 
     while(!window.ShouldClose()) {
         window.BeginDrawing();
