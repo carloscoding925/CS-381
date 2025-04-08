ca0354ecb7b268b873c0ab7d704ecf7fbcd236ef
Carlos Hernandez <carlosandresh925@gmail.com>
2025-04-07 20:56:48 -0700

    bruh

--- as8/src/as8.cpp
+++ as8/src/as8.cpp
@@ -1,25 +1,90 @@
 #include <raylib-cpp.hpp>
+#include <BufferedRaylib.hpp>
 #include "Window.hpp"
 #include "../assets/skybox.hpp"
 #include <iostream>
 #include "ECS.hpp"
 
-struct Scene {
-    struct PhysicsComponent {
-        raylib::Vector3 position;
-        raylib::Vector3 velocity;
-    };
+size_t globalComponentCounter = 0;
 
-    PhysicsComponent physics[10];
-    float heading[10];
-    raylib::Model* model[10];
+template <typename T>
+concept Transformer = requires(T t, raylib::Matrix m) {
+    { t(m) } -> std::convertible_to<raylib::Matrix>;
 };
 
-void kinematic_physics(Scene& scene, float dt) {
-    for (int e = 0; e < 10; ++e) {
-        scene.physics[e].position = scene.physics[e].position + scene.physics[e].velocity * dt;
-    }
+void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Matrix backup = model.transform;
+    model.transform = transformer(backup);
+    model.Draw({});
+    model.GetTransformedBoundingBox().Draw(raylib::Color::White());
+    model.transform = backup;
 }
+
+void DrawModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Matrix backup = model.transform;
+    model.transform = transformer(backup);
+    model.Draw({});
+    model.transform = backup;
+}
+
+struct RenderComponent {
+    raylib::Model* model;
+    bool drawBoundingBox = false;
+};
+
+struct TransformComponent {
+    raylib::Vector3 position = {0.0f, 0.0f, 0.0f};
+    raylib::Degree heading = 0;
+};
+
+struct KinematicPhysicsComponent {
+    raylib::Vector3 velocity = {0.0f, 0.0f, 0.0f};
+    float speed = 0.0f;
+    float targetSpeed = 0.0f;
+};
+
+struct TwoDeePhysicsComponent {
+    raylib::Degree targetHeading = 0;
+    float maxSpeed = 100.0f;
+    float acceleration = 5.0f;
+    float turningRate = 5.0f;
+};
+
+struct ThreeDeeComponent {
+    float maxSpeed = 100.0f;
+    float acceleration = 5.0f;
+    float turningRate = 5.0f;  
+};
+
+void DrawAllModels(cs381::Scene<cs381::ComponentStorage>& scene, int selectedEntity)
+{
+    for (cs381::Entity e = 0; e < scene.entityMasks.size(); e++)
+    {
+        if(!scene.HasComponent<RenderComponent>(e)) continue;
+		if(!scene.HasComponent<TransformComponent>(e)) continue;
+
+        auto& rendering = scene.GetComponent<RenderComponent>(e);
+		auto& transform = scene.GetComponent<TransformComponent>(e);
+
+		if(scene.HasComponent<KinematicPhysicsComponent>(e))
+		{
+			if(selectedEntity == e)
+			{
+				DrawBoundedModel(*rendering.model, [&transform](const raylib::Transform& m) -> raylib::Transform {
+					return m.Translate(transform.position).Scale(30);
+				});
+			}
+			else
+			{
+				DrawModel(*rendering.model, [&transform](const raylib::Transform& m) -> raylib::Transform {
+					return m.Translate(transform.position).Scale(30);
+				});
+			}
+
+		}
+	}
+}
+
 
 int main() {
     const int windowWidth = 1000;
@@ -31,20 +96,73 @@ int main() {
     cs381::SkyBox sky("textures/skybox.png");
     raylib::Camera3D camera(raylib::Vector3{0.0f, 120.0f, -500.0f}, raylib::Vector3{0.0f, 0.0f, 0.0f}, raylib::Vector3{0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE);
 
+    raylib::Model rocket("../assets/Kenny Space Kit/rocketA.glb");
+    raylib::Model truck("../assets/Kenny Car Kit/truck.glb");
+    raylib::Model ambulance("../assets/Kenny Car Kit/ambulance.glb");
+    raylib::Model garbageTruck("../assets/Kenny Car Kit/garbage-truck.glb");
+    raylib::Model sportsSedan("../assets/Kenny Car Kit/sedan-sports.glb");
+    raylib::Model fireTruck("../assets/Kenny Car Kit/firetruck.glb");
+
+    int selectedEntity = 0;
+    raylib::BufferedInput bufferedInput;
+
+    bufferedInput["Forward"] = raylib::Action::key(KEY_W).move();
+    bufferedInput["SlowDown"] = raylib::Action::key(KEY_S).move();
+    bufferedInput["Left"] = raylib::Action::key(KEY_A).move();
+    bufferedInput["Right"] = raylib::Action::key(KEY_D).move();
+    bufferedInput["Stop"] = raylib::Action::key(KEY_SPACE).move();
+    bufferedInput["Switch"] = raylib::Action::key(KEY_TAB).SetPressedCallback([&selectedEntity]{
+        if (selectedEntity < 6) {
+            selectedEntity++;
+        } else {
+            selectedEntity = 0;
+        }
+    }).move();
+
     cs381::Scene<cs381::ComponentStorage> scene;
-    auto e = scene.CreateEntity();
-    scene.AddComponent<raylib::Vector3>(e);
+    
+    cs381::Entity rocketEntity = scene.CreateEntity();
+    scene.AddComponent<RenderComponent>(rocketEntity);
+    scene.GetComponent<RenderComponent>(rocketEntity).model = &rocket;
+    scene.AddComponent<TransformComponent>(rocketEntity);
+    scene.AddComponent<KinematicPhysicsComponent>(rocketEntity);
+    scene.AddComponent<TwoDeePhysicsComponent>(rocketEntity);
+    scene.AddComponent<ThreeDeeComponent>(rocketEntity);
+    
+    cs381::Entity truckEntity = scene.CreateEntity();
+    scene.AddComponent<RenderComponent>(truckEntity);
+    scene.GetComponent<RenderComponent>(truckEntity).model = &truck;
+    scene.AddComponent<TransformComponent>(truckEntity);
+    scene.AddComponent<KinematicPhysicsComponent>(truckEntity);
+    scene.AddComponent<TwoDeePhysicsComponent>(truckEntity);
 
-    /*for(cs381::Entity e = 0; e < scene.entityMasks.size(); ++e) {
-        if (!scene.HasComponent<Position>(e)) {
-            continue;
-        }
-        if (!scene.HasComponent<Velocity>(e)) {
-            continue;
-        }
+    cs381::Entity ambulanceEntity = scene.CreateEntity();
+    scene.AddComponent<RenderComponent>(ambulanceEntity);
+    scene.GetComponent<RenderComponent>(ambulanceEntity).model = &ambulance;
+    scene.AddComponent<TransformComponent>(ambulanceEntity);
+    scene.AddComponent<KinematicPhysicsComponent>(ambulanceEntity);
+    scene.AddComponent<TwoDeePhysicsComponent>(ambulanceEntity);
 
-        // do physics on e
-    }*/
+    cs381::Entity garbageTruckEntity = scene.CreateEntity();
+    scene.AddComponent<RenderComponent>(garbageTruckEntity);
+    scene.GetComponent<RenderComponent>(garbageTruckEntity).model = &garbageTruck;
+    scene.AddComponent<TransformComponent>(garbageTruckEntity);
+    scene.AddComponent<KinematicPhysicsComponent>(garbageTruckEntity);
+    scene.AddComponent<TwoDeePhysicsComponent>(garbageTruckEntity);
+
+    cs381::Entity sportsSedanEntity = scene.CreateEntity();
+    scene.AddComponent<RenderComponent>(sportsSedanEntity);
+    scene.GetComponent<RenderComponent>(sportsSedanEntity).model = &sportsSedan;
+    scene.AddComponent<TransformComponent>(sportsSedanEntity);
+    scene.AddComponent<KinematicPhysicsComponent>(sportsSedanEntity);
+    scene.AddComponent<TwoDeePhysicsComponent>(sportsSedanEntity);
+
+    cs381::Entity fireTruckEntity = scene.CreateEntity();
+    scene.AddComponent<RenderComponent>(fireTruckEntity);
+    scene.GetComponent<RenderComponent>(fireTruckEntity).model = &fireTruck;
+    scene.AddComponent<TransformComponent>(fireTruckEntity);
+    scene.AddComponent<KinematicPhysicsComponent>(fireTruckEntity);
+    scene.AddComponent<TwoDeePhysicsComponent>(fireTruckEntity);
 
     while(!window.ShouldClose()) {
         window.BeginDrawing();
@@ -53,6 +171,7 @@ int main() {
             {
                 window.ClearBackground(raylib::Color::White());
                 sky.Draw();
+                DrawAllModels(scene, selectedEntity);
             }
             camera.EndMode();
         }
